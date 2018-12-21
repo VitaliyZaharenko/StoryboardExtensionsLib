6ed642e1818f459df8510dcb3b893f73cbebcc14
VitaliZahar <vitalizahar@gmail.com>
2018-12-21 11:03:51 -0800

    Initial Commit

---
+++ StoryboardExtensions/StoryboardExtensions.h
@@ -0,0 +1,19 @@
+//
+//  StoryboardExtensions.h
+//  StoryboardExtensions
+//
+//  Created by vitali on 12/21/18.
+//  Copyright © 2018 vitcopr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StoryboardExtensions.
+FOUNDATION_EXPORT double StoryboardExtensionsVersionNumber;
+
+//! Project version string for StoryboardExtensions.
+FOUNDATION_EXPORT const unsigned char StoryboardExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StoryboardExtensions/PublicHeader.h>
+
+
